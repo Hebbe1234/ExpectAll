124be60234494aed3442f2f5e5f33feef1838eae
Gustav Sneholm Bruhns <gustavsneholmbruhns@h379.aau1x.klient.slv.site.aau.dk>
2023-10-23 12:40:04 +0200

    reordering

--- src/bdd.py
+++ src/bdd.py
@@ -58,7 +58,7 @@ class BDD:
 
     }
 
-    def __init__(self, topology: MultiDiGraph, demands: dict[int, Demand], wavelengths: int = 2):
+    def __init__(self, topology: MultiDiGraph, demands: dict[int, Demand], ordering: list[ET], wavelengths: int = 2, other_order:bool = False, generics_first:bool = False):
         self.bdd = _BDD()
         self.variables = []
         self.node_vars = {v:i for i,v in enumerate(topology.nodes)}
@@ -80,80 +80,96 @@ class BDD:
 
         }
         self.bdd.configure(reordering=False)
-        self.gen_vars()
-
-
+        self.gen_vars(ordering, other_order, generics_first)
 
         levels = {var: self.bdd.level_of_var(var) for var in list(self.bdd.vars)}
 
-        print("")
-        print("")
-        print("")
+        # print("")
+        # print("")
+        # print("")
 
-        for elem in sorted(levels.items(), key= lambda k: k[1]):
-            print(elem)
+        # for elem in sorted(levels.items(), key= lambda k: k[1]):
+        #     print(elem)
     
-        print("")
-        print("")
-        print("")
+        # print("")
+        # print("")
+        # print("")
     
-    def gen_vars(self):
+    # Demands, Paths, Lambdas, Edges, Nodes (T, N, S)
+    def gen_vars(self, ordering: list[ET], other_order: bool = False, generic_first:bool = False):
+        for type in ordering:
+            match type:
+                case BDD.ET.DEMAND:
+                    self.declare_variables(BDD.ET.DEMAND)
+                    self.declare_variables(BDD.ET.DEMAND, 2)
+
+                case BDD.ET.PATH:
+                    self.declare_generic_and_specific_variables(BDD.ET.PATH, list(self.edge_vars.values()), other_order, generic_first)
+
+                case BDD.ET.LAMBDA:
+                    self.declare_generic_and_specific_variables(BDD.ET.LAMBDA,  list(range(1, 1 + self.encoding_counts[BDD.ET.LAMBDA])), other_order, generic_first)
+
+                case BDD.ET.EDGE:
+                    self.declare_variables(BDD.ET.EDGE)
+                    self.declare_variables(BDD.ET.EDGE, 2)
+                
+                case BDD.ET.NODE | BDD.ET.SOURCE | BDD.ET.TARGET:
+                    self.declare_variables(type)
+                
+                case _: 
+                    raise Exception(f"Error: the given type {type} did not match any BDD type.")
+                    
+    # Vertices (N, S, T), edges, demands, paths, lambda
+    def gen_vars_2(self):
+        self.declare_variables(BDD.ET.NODE)
+        self.declare_variables(BDD.ET.SOURCE)
+        self.declare_variables(BDD.ET.TARGET)
         
-        dd_bdd_vars = [f"{BDD.prefixes[BDD.ET.DEMAND]}{self.encoding_counts[BDD.ET.DEMAND] - i}" for i in range(0,self.encoding_counts[BDD.ET.DEMAND])]
-        self.bdd.declare(*dd_bdd_vars)
-
-        d_bdd_vars = [f"{self.get_prefix_multiple(BDD.ET.DEMAND, 2)}{self.encoding_counts[BDD.ET.DEMAND] - i}" for i in range(0,self.encoding_counts[BDD.ET.DEMAND])]
-        self.bdd.declare(*d_bdd_vars)
+        self.declare_variables(BDD.ET.EDGE)
+        self.declare_variables(BDD.ET.EDGE, 2)
+        
+        self.declare_variables(BDD.ET.DEMAND)
+        self.declare_variables(BDD.ET.DEMAND, 2)
         
         self.declare_generic_and_specific_variables(BDD.ET.PATH, list(self.edge_vars.values()))
         self.declare_generic_and_specific_variables(BDD.ET.LAMBDA,  list(range(1, 1 + self.encoding_counts[BDD.ET.LAMBDA])))
 
-        
-        edge_encoding_count = math.ceil(math.log2(len(self.edge_vars.keys())))
-
-        e_bdd_vars = [f"{BDD.prefixes[BDD.ET.EDGE]}{edge_encoding_count - i }" for i in range(0,edge_encoding_count)] 
-        self.bdd.declare(*e_bdd_vars)
-
-        
-        ee_bdd_vars = [f"{self.get_prefix_multiple(BDD.ET.EDGE, 2)}{self.encoding_counts[BDD.ET.EDGE]  - i }" for i in range(0,self.encoding_counts[BDD.ET.EDGE] )]
-        self.bdd.declare(*ee_bdd_vars)
-        
-        variable_encoding_count = math.ceil(math.log2(len(self.node_vars.keys())))
-        
-        t_bdd_vars = [f"{BDD.prefixes[BDD.ET.TARGET]}{variable_encoding_count - i }" for i in range(0,variable_encoding_count)] 
-        self.bdd.declare(*t_bdd_vars)
-        self.encoded_target_vars = t_bdd_vars
-        
-        v_bdd_vars = [f"{BDD.prefixes[BDD.ET.NODE]}{variable_encoding_count - i }" for i in range(0,variable_encoding_count)] 
-        self.bdd.declare(*v_bdd_vars)
-        self.encoded_node_vars = v_bdd_vars
-
-        s_bdd_vars = [f"{BDD.prefixes[BDD.ET.SOURCE]}{variable_encoding_count - i }" for i in range(0,variable_encoding_count)] 
-        self.bdd.declare(*s_bdd_vars)
-        self.encoded_source_vars = s_bdd_vars
 
 
-  
-
-
-
-        
-        demand_encoding_count = math.ceil(math.log2(len(self.demand_vars.keys())))
-        d_bdd_vars = [f"{BDD.prefixes[BDD.ET.DEMAND]}{demand_encoding_count - i}" for i in range(0,demand_encoding_count)] 
+    def declare_variables(self, type: ET, prefix_count: int = 1):
+        d_bdd_vars = [f"{self.get_prefix_multiple(type, prefix_count)}{self.encoding_counts[type] - i}" for i in range(0,self.encoding_counts[type])]
         self.bdd.declare(*d_bdd_vars)
         
+        return d_bdd_vars
+        
 
-    def declare_generic_and_specific_variables(self, type: ET, l: list[int]):
+    def declare_generic_and_specific_variables(self, type: ET, l: list[int], other_order=False, generic_first=False):
         bdd_vars = []
-        for demand in self.demand_vars.keys():
+
+        def append(item, demand):
+            bdd_vars.append(f"{BDD.prefixes[type]}{item}_{demand}")
+            bdd_vars.append(f"{self.get_prefix_multiple(type,2)}{item}_{demand}")
+        
+        def gen_generics():
             for item in l:
-                bdd_vars.append(f"{BDD.prefixes[type]}{item}_{demand}")
-                bdd_vars.append(f"{self.get_prefix_multiple(type,2)}{item}_{demand}")
-
-        for item in l:
-            bdd_vars.append(f"{BDD.prefixes[type]}{item}")
-            bdd_vars.append(f"{self.get_prefix_multiple(type,2)}{item}")
-
+                bdd_vars.append(f"{BDD.prefixes[type]}{item}")
+                bdd_vars.append(f"{self.get_prefix_multiple(type,2)}{item}")
+        
+        if generic_first:
+            gen_generics()
+        
+        if other_order:
+            for item in l:
+                for demand in self.demand_vars.keys():
+                    append(item, demand)
+        else:
+            for demand in self.demand_vars.keys():
+                for item in l:
+                    append(item, demand)
+                
+        if not generic_first:
+            gen_generics()
+        
         self.bdd.declare(*bdd_vars)
 
 
@@ -502,7 +518,7 @@ class RoutingAndWavelengthBlock(Block):
             
             i_l = i
             
-            print(f"{i}/{len(d_expr)}")
+            #print(f"{i}/{len(d_expr)}")
             d_e1 = d_expr[i]
             d_e2 = d_expr[i+1]
             d_e3 = d_expr[i+2]
@@ -511,16 +527,16 @@ class RoutingAndWavelengthBlock(Block):
         
         for j in range(i_l + 1, len(d_expr)):
             
-            print(f"{j}/{len(d_expr)}")
+            #print(f"{j}/{len(d_expr)}")
             d_e = d_expr[j] 
             self.expr = (self.expr & d_e)  
             
 from timeit import default_timer as timer
 
 class RWAProblem:
-    def __init__(self, G: MultiDiGraph, demands: dict[int, Demand], wavelengths: int):
+    def __init__(self, G: MultiDiGraph, demands: dict[int, Demand], ordering: list[BDD.ET], wavelengths: int, other_order: bool = False, generics_first: bool = False):
         s = timer()
-        self.base = BDD(G, demands, wavelengths)
+        self.base = BDD(G, demands, ordering, wavelengths, other_order, generics_first)
 
         in_expr = InBlock(G, self.base)
         out_expr = OutBlock(G, self.base)
@@ -537,7 +553,6 @@ class RWAProblem:
         demandPath = DemandPathBlock(path,source,target,singleIn, singleOut,self.base)
         singleWavelength_expr = SingleWavelengthBlock(self.base)
         noClash_expr = NoClashBlock(passes_expr, self.base) 
-        print(demandPath.expr.count())
 
         e1 = timer()
         print(e1 - s, flush=True)
@@ -545,6 +560,7 @@ class RWAProblem:
         self.rwa = RoutingAndWavelengthBlock(demandPath, singleWavelength_expr, noClash_expr, len(demands), self.base )
         e2 = timer()
         print(e2 - s, flush=True)
+        print("")
         
     def get_assignments(self):
         return get_assignments(self.base.bdd, self.rwa.expr)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import topology
-from bdd import RWAProblem, pretty_print
+from bdd import RWAProblem, pretty_print, BDD
 from demands import Demand
 import networkx as nx 
+from itertools import permutations
+
 if __name__ == "__main__":
     G = topology.get_nx_graph(topology.TOPZOO_PATH +  "/AI3.gml")
     G = nx.MultiDiGraph(nx.nx_pydot.read_dot("../dot_examples/four_node.dot"))
@@ -9,19 +11,29 @@ if __name__ == "__main__":
     if G.nodes.get("\\n") is not None:
         G.remove_node("\\n")
         
-    demands = topology.get_demands(G, 10)
+    demands = topology.get_demands(G, 2)
     demands = {0: Demand("A", "B"), 
                1: Demand("B", "D"), 
                2: Demand("C", "B"), 
                3: Demand("A", "B"),
                4: Demand("A", "D"),
-               5: Demand("B", "A"),
-               6: Demand("C", "B"),
-               7: Demand("D", "B"),
-               8: Demand("A", "C"),
-               9: Demand("B", "A")}
+                5: Demand("B", "A"),
+                6: Demand("C", "B"),
+                7: Demand("D", "B"),
+                8: Demand("A", "C"),
+            #    9: Demand("B", "A")
+               }
     print(demands)
-    rwa = RWAProblem(G, demands, 5)
-    print(rwa.rwa.expr.count())
+    
+    ordering = [BDD.ET.DEMAND, BDD.ET.PATH, BDD.ET.EDGE, BDD.ET.SOURCE, BDD.ET.TARGET, BDD.ET.NODE]
+    p = permutations(ordering)
+    
+    for i,o in enumerate(p):
+        print(f"ordering being checked: {o}")
+        # rwa = RWAProblem(G, demands, [*o], 5, other_order =False, generics_first=False)
+        # rwa = RWAProblem(G, demands, [*o], 5, other_order =False, generics_first=True)
+        rwa = RWAProblem(G, demands, [BDD.ET.LAMBDA]+[*o], 5, other_order =True, generics_first=False)
+        rwa = RWAProblem(G, demands, [BDD.ET.LAMBDA]+[*o], 5, other_order =True, generics_first=True)
+        print(rwa.rwa.expr.count())
     
     #rwa.print_assignments(true_only=True, keep_false_prefix="l")
