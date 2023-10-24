679afa6cb84992bc67ffabd74b4807b0fd08dfaa
fhyldg18 <57632309+fhyldg18@users.noreply.github.com>
2023-10-24 10:51:13 +0200

    seeding demands

--- batchScripts/run_all_mips.sh
+++ batchScripts/run_all_mips.sh
@@ -14,19 +14,21 @@ mkdir out/mip
 
 cd src 
 
-python3.9 -m venv venv #change to actual version we want to use
+python3.9 -m venv venv #TODO change to actual version we want to use
 source venv/bin/activate
 pip install pulp networkx matplotlib pydot
 deactivate
 
 cd ../batchScripts
 
+num_demands=(1) #array of number of demands to run
+
 for file in "$DIR"/*
 do
 
     GML_NAME=$(basename $file)    
 
-    bash ./run_single_mip_for_many_demand.sh $GML_NAME 1 2 #array of demands, change to run on precomputed demands
+    bash ./run_single_mip_for_many_demand.sh $GML_NAME $num_demands
 done
 
 # Additional commands or post-processing can go here
--- src/mip.py
+++ src/mip.py
@@ -1,3 +1,7 @@
+
+#!/usr/bin/env python
+
+
 import pulp
 import pulp.apis
 import networkx as nx
@@ -5,8 +9,11 @@ from networkx import digraph
 from networkx import MultiDiGraph
 from demands import Demand
 from topology import get_demands
+from topology import get_nx_graph
 import sys
+import argparse
 
+ 
 
 def z_lookup(wavelenth : int):
     return "l"+str(wavelenth)
@@ -16,113 +23,116 @@ def y_lookup(wavelenth : int, demand : int):
 
 def x_lookup(wavelenth : int, edge : int, demand : int):
     return "l"+str(wavelenth)+"_e"+str(edge)+"_d"+str(demand)
-
-def match_edges(topology, e): 
-    for jj, ee in enumerate(topology.edges): 
-        if e[0] == ee[0] and e[1] == ee[1]: 
+ 
+def match_edges(topology, e):
+    for jj, ee in enumerate(topology.edges):
+        if e[0] == ee[0] and e[1] == ee[1]:
             return jj
     assert("error")
     print("sdfsefsefsefse\n\n\n\n")
     exit()
     return -1
 
-def SolveUsingMIP(topology: MultiDiGraph, demands: list[Demand], wavelengths : int):
 
-    z_var_dict = pulp.LpVariable.dicts('z', 
+def SolveUsingMIP(topology: MultiDiGraph, demands: list[Demand], wavelengths : int):
+ 
+
+    z_var_dict = pulp.LpVariable.dicts('z',
                                        [ ("l"+str(i))for i in range(wavelengths)], lowBound=0, upBound=1, cat='Integer')
-    y_var_dict = pulp.LpVariable.dicts('y', 
-                                       [("l"+str(wl) + "_" + "d"+str(d)) 
-                                        for wl  in range(wavelengths) 
+    y_var_dict = pulp.LpVariable.dicts('y',
+                                       [("l"+str(wl) + "_" + "d"+str(d))
+                                        for wl  in range(wavelengths)
                                         for d in range(len(demands))], lowBound=0, upBound=1, cat="Integer")
     x_var_dict = pulp.LpVariable.dicts('x',  
                                        [("l"+str(wl) + "_" + "e"+str(e) + "_"+"d" + str(d)) for wl  in range(wavelengths)
-                                               for d in range(len(demands)) 
+                                               for d in range(len(demands))
                                                for e, _ in enumerate(topology.edges)],
                                                  lowBound=0, upBound=1, cat="Integer")
-
+ 
     #8
     # Define the PuLP problem and set it to minimize
     prop = pulp.LpProblem('MyProblems:)', pulp.LpMinimize)
-
+ 
     # Define the objective function to minimize the sum of z_var_dict values
     objective = pulp.lpSum(z_var_dict.values())
 
     # Add the objective function to the problem
     prop += objective
-
+ 
 
     #10
-    for d in range(len(demands)) : 
+    for d in range(len(demands)) :
         sum = 0
-        for w in range(wavelengths): 
+        for w in range(wavelengths):
             sum += y_var_dict[y_lookup(w,d)]
         prop += sum == 1
 
     #11
-    for d in range(len(demands)): 
-        for e, _ in enumerate(topology.edges): 
+    for d in range(len(demands)):
+        for e, _ in enumerate(topology.edges):
             for w in range(wavelengths):
                 prop += x_var_dict[x_lookup(w,e,d)] <= z_var_dict[z_lookup(w)]
 
     #12
-    for e, _ in enumerate(topology.edges): 
+    for e, _ in enumerate(topology.edges):
         for w in range(wavelengths):
             sum = 0
             for d in range(len(demands)):
-                sum += x_var_dict[x_lookup(w,e,d)] 
+                sum += x_var_dict[x_lookup(w,e,d)]
             prop += sum <= 1
-
-
+ 
     #13
     for i, d in enumerate(demands):
         for w in range(wavelengths):
             y_sum = 0
-            for j, e in enumerate(topology.in_edges(d.source)): 
+            for j, e in enumerate(topology.in_edges(d.source)):
                 kk = match_edges(topology,e)
                 y_sum += x_var_dict[x_lookup(w,kk,i)]
             prop += y_sum == 0
             x_sum = 0
             #Makes infeasable
-            for l, e in enumerate(topology.out_edges(d.source)): 
+            for l, e in enumerate(topology.out_edges(d.source)):
                 kk = match_edges(topology,e)
                 x_sum += x_var_dict[x_lookup(w,kk,i)]
             prop += x_sum == y_var_dict[y_lookup(w,i)]
-    
+   
     #14
     for i, d in enumerate(demands):
         for w in range(wavelengths):
             y_sum = 0
-            for j, e in enumerate(topology.out_edges(d.target)): 
+            for j, e in enumerate(topology.out_edges(d.target)):
                 kk = match_edges(topology,e)
                 y_sum += x_var_dict[x_lookup(w,kk,i)]
             prop += y_sum == 0
             x_sum = 0
-            for j, e in enumerate(topology.in_edges(d.target)): 
+            for j, e in enumerate(topology.in_edges(d.target)):
                 kk = match_edges(topology,e)
                 x_sum += x_var_dict[x_lookup(w,kk,i)]
             prop += x_sum == y_var_dict[y_lookup(w,i)]
+ 
 
     #15
     for i, d in enumerate(demands):
         for w in range(wavelengths):
-            for j, n in enumerate(topology.nodes): 
-                if n == d.source or n == d.target : 
+            for j, n in enumerate(topology.nodes):
+                if n == d.source or n == d.target :
                     continue
-
+ 
                 in_sum = 0
-                for j, e in enumerate(topology.in_edges(n)): 
+                for j, e in enumerate(topology.in_edges(n)):
                     kk = match_edges(topology,e)
                     in_sum += x_var_dict[x_lookup(w,kk,i)]
                 out_sum = 0
-                for j, e in enumerate(topology.out_edges(n)): 
+                for j, e in enumerate(topology.out_edges(n)):
                     kk = match_edges(topology,e)
                     out_sum += x_var_dict[x_lookup(w,kk,i)]                
-                prop += in_sum == out_sum 
+                prop += in_sum == out_sum
+ 
 
     # status = prop.solve(pulp.apis.GUROBI(mas=True, gapRel=0.01,timeLimit=300))
-
+ 
     status = prop.solve()
-    if pulp.constants.LpStatusInfeasible == status: 
+    if pulp.constants.LpStatusInfeasible == status:
         print("Infeasable :(")
     # Print the results
     # print("Status:", pulp.LpStatus[prop.status])
@@ -130,16 +140,29 @@ def SolveUsingMIP(topology: MultiDiGraph, demands: list[Demand], wavelengths : i
     # Print all variable values
     # for var in prop.variables():
     #     print(f"{var.name} = {var.varValue}")
+
         # Solve the linear programming problem
 
+ 
+
 def main():
-    G = nx.MultiDiGraph(nx.nx_pydot.read_dot("../dot_examples/simple_net.dot"))
+    print("shdfhsdf")
+    parser = argparse.ArgumentParser("mainmip.py")
+    parser.add_argument("--filename", type=str, help="dfd")
+    parser.add_argument("--wavelengths", default=10, type=int, help="wfvdf")
+    parser.add_argument("--demands", default=10, type=int, help="dedf")
+    args = parser.parse_args()
+    print(args)
+    G = get_nx_graph("./topologies/topzoo/"+args.filename)
     if G.nodes.get("\\n") is not None:
         G.remove_node("\\n")
-    demands = get_demands(G, 10)
+    demands = get_demands(G, args.demands)
     demands = list(demands.values())
-    SolveUsingMIP(G, demands, 5)
-
+    SolveUsingMIP(G, demands, args.wavelengths)
+    
 if __name__ == "__main__":
     main()
 
+ 
+
+ 
