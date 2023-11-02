3985a0d7a1d36b262627c8fb74b84aafe173dbb8
fhyldg18 <57632309+fhyldg18@users.noreply.github.com>
2023-11-02 08:46:21 +0100

    in progress

--- src/mkplot/AAU_create_json_and_make_pdfs.py
+++ src/mkplot/AAU_create_json_and_make_pdfs.py
@@ -1,36 +1,21 @@
 import os
 import json
-from plot import Plot
 import subprocess
+import math
 
-# Initialize a dictionary to store the data
-
-data = {}
-
-# Set the directory path where your data is located
-data_directory = "../../out/mip" #TODO Change this so it points the correct way :)
-
-text_file_count = 0
-for root, dirs, files in os.walk(data_directory):
-    for directory in dirs:
-        directory_path = os.path.join(root, directory)
-        
-        # Iterate through files in the subdirectory
-        for filename in os.listdir(directory_path):
-            if filename.endswith(".txt"):
-                number_of_demands = filename.split("output")[1].split(".txt")[0]
-                data[str(number_of_demands)] = {"stats":{}}
-        break
-    break
-    
-
-# Function to parse the content of a text file
+# Extract solve times from output files, if they were solved
 def parse_txt_file(file_path):
     with open(file_path, "r") as file:
         lines = file.readlines()
 
-    if len(lines) >= 2:
-        solve_time, all_time, solvable = map(str.strip, lines[1].split(","))
+    #if  lines and "solve" in lines[-1]:
+     #   solve_time = map(str.strip, lines[-1].split(":"))
+    if not lines:
+        return None
+    
+    solve_time, all_time, solveable = map(str.strip, lines[-1].split(","))
+
+    if solveable:
         return {
             "status": True,
             "rtime": float(solve_time),
@@ -38,76 +23,118 @@ def parse_txt_file(file_path):
         }
     else:
         return None
+    
+# initialize data for each run of demands. Demands are given in the names of the output files.
+def init_data(data_directory):
+    data = {}
 
-# Recursively walk through the directory and its subdirectories
-for root, dirs, files in os.walk(data_directory):
-    instanceNumber = 0
-    for directory in dirs:
-        instanceNumber += 1
-        directory_path = os.path.join(root, directory)
-        for filename in os.listdir(directory_path):
-            file_path = os.path.join(directory_path,filename)
+    for root, graph_dirs,_ in os.walk(data_directory):
+        for graph_dir in graph_dirs:
+            directory_path = os.path.join(root, graph_dir)
+            
+            # Iterate through files in the subdirectory
+            for output in os.listdir(directory_path):
+                if output.endswith(".txt"):
+                    number_of_demands = output.split("output")[1].split(".txt")[0]
+                    data[str(number_of_demands)] = {"stats":{}}
+            break
+        break
+    return data
 
-            number_of_demands = filename.split("output")[1].split(".txt")[0]
+# Extract running times for each graph for each demand
+def extract_run_times(data_directory,data):
+    for root, graph_dirs, _ in os.walk(data_directory):
+        instance = 0
+        for graph_dir in graph_dirs:
+            instance += 1
+            directory_path = os.path.join(root, graph_dir)
+            for output in os.listdir(directory_path):
+                output_path = os.path.join(directory_path,output)
 
-            # Use the file name (without extension) as the dictionary key
-            instance_name = os.path.splitext("instance"+str(instanceNumber))[0]
-            # Parse the content of the text file
-            instance_data = parse_txt_file(file_path)
+                number_of_demands = output.split("output")[1].split(".txt")[0]
+                instance_name = os.path.splitext("instance"+str(instance))[0]
+                instance_data = parse_txt_file(output_path)
 
-            if instance_data is not None:
-                data[str(number_of_demands)]["stats"][instance_name] = instance_data
+                if instance_data is not None:
+                    data[str(number_of_demands)]["stats"][instance_name] = instance_data
 
+# Define graphing metadata
+def init_graph_metadata(data):
+    for key, section in data.items():
+        if not section["stats"]: #if all runs timed out
+            continue
 
-# Convert the data dictionary to a JSON object
-json_data = json.dumps(data, indent=4)
+        y_min,y_max,x_min,x_max = math.inf,0,0,0
+        for k,d in section.items():
+            
+            for j, l in d.items(): 
+                if l["rtime"] > y_max : 
+                    y_max = l["rtime"]
+                if l["rtime"] < y_min : 
+                    y_min = l["rtime"]
+                x_max += 1
 
-
-
-# Print or save the JSON data as needed
-
-
-# Define the preamble information
-import math
-
-# Iterate through your data dictionary and update each section with the preamble
-for key, section in data.items():
-    y_min,y_max,x_min,x_max = math.inf,0,0,0
-    for k,d in section.items():
-         
-        for j, l in d.items(): 
-            if l["rtime"] > y_max : 
-                y_max = l["rtime"]
-            if l["rtime"] < y_min : 
-                y_min = l["rtime"]
-            x_max += 1
-
-    meta_info = {
-        "meta":{
-            "y_min": y_min, 
-            "y_max": y_max,
-            "x_min": x_min,
-            "x_max": x_max
+        meta_info = {
+            "meta":{
+                "y_min": y_min, 
+                "y_max": y_max,
+                "x_min": x_min,
+                "x_max": x_max
+            }
         }
-    }
-    preamble_info = {
-    "preamble": {
-        "benchmark": "ttt" ,
-        "prog_args": None,
-        "program": "another-good-tool",
-        "prog_alias": "number of demands :)" + str(key)
+        preamble_info = {
+        "preamble": {
+            "benchmark": "ttt" ,
+            "prog_args": None,
+            "program": "another-good-tool",
+            "prog_alias": "number of demands" + str(key)
+            }
         }
-    }
 
-    section.update(preamble_info)
-    section.update(meta_info)
-    json_data = json.dumps(section, indent=4)
+        section.update(preamble_info)
+        section.update(meta_info)
 
-    # Create a JSON file for each section
-    file_name = f"json_folder/plotWithDemand{key}.json"
-    with open(file_name, "w") as json_file:
-        json.dump(section, json_file, indent=4)
-    # Define the command as a list
+
+out_dirs = ["mip", "mytest2"]
+full_data = {}
+
+for out in out_dirs:
+    data = {}
+    data_directory = f"../../out/{out}" #TODO Change this so it points the correct way :)
+    data = init_data(data_directory)
+    extract_run_times(data_directory, data)
+    init_graph_metadata(data)
+
+    for demands, _ in data.items():
+        full_data[demands] = {}
+
+    for demands, plot_data in data.items():
+        file_name = f"json_folder/{demands}_{out}.json"
+        with open(file_name, "w") as json_file:
+            json.dump(plot_data, json_file, indent=4)
+        full_data[demands].update({out:plot_data})
+
+exit()
+for demand, out_dirs in full_data.items():
+    # xmax = []
+    # ymax = []
+    # ymin = []
+    # xmin = []
+    
+    
+    for out in out_dirs:
+        if not out_dirs[out]["stats"]:
+            break
+        xmax = [out_dirs[out]["meta"]["x_max"] for out in out_dirs if "meta" in out_dirs[out].keys()]
+        ymax = [out_dirs[out]["meta"]["y_max"] for out in out_dirs if "meta" in out_dirs[out].keys()]
+        xmin = [out_dirs[out]["meta"]["x_min"] for out in out_dirs if "meta" in out_dirs[out].keys()]
+        ymin = [out_dirs[out]["meta"]["y_max"] for out in out_dirs if "meta" in out_dirs[out].keys()]
+        
+        xmax = max(xmax)
+        ymax= max(ymax)
+        ymin = min(ymin)
+        xmin = min(xmin)
+    print(xmax, ymax, ymin, xmin)
     command = [
         'python3',      # The Python interpreter
         'mkplot.py',    # The script you want to run
@@ -115,16 +142,15 @@ for key, section in data.items():
         '-t', '1000000',
         '-b', 'pdf',
         '--ylog',
-        '--save-to', 'asd'+str(key) + '.pdf',
-        '--xmax', str(x_max),
-        '--ymin', str(y_min),
-        '--ymax', str(y_max),
+        '--save-to', 'demands'+str(demand) + '.pdf',
+        '--xmax', str(xmax),
+        '--ymin', str(ymin),
+        '--ymax', str(ymax),
         '--lloc', 'lower right',
-        file_name
+        f"./json_folder/*_mip.json"
     ]
-
-    # Run the command
     subprocess.run(command)
+
 
     
 
