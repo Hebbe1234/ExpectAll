626fd62f96b533bcdf91d9d0c04644bd6af061e0
fhyldg18 <57632309+fhyldg18@users.noreply.github.com>
2023-11-13 13:18:57 +0100

    new experiment

--- batchScripts/run_experiment.sh
+++ batchScripts/run_experiment.sh
@@ -18,6 +18,8 @@ case $EXPERIMENT in
 	
 	13) #wavelengths experiment
 		bash run_all.sh ../src ../src/topologies/topzoosynth_for_wavelengths/ ../src/topologies/wavelengths.txt ../out/bdd_wavelengths_static_demands_run$RUN run_bdd.py wavelengths_static_demands 10 10 1 1 $BASHFILE;;
+	13.2) #wavelengths experiment
+		bash run_all.sh ../src ../src/topologies/topzoosynth_for_wavelengths/ ../src/topologies/wavelengths.txt ../out/bdd_wavelengths_static_demands_run$RUN run_bdd.py wavelengths_static_demands 10 20 1 1 $BASHFILE;;
 
 	20) #mip, default
 		bash run_all.sh ../src ../src/topologies/topzoo/ ../src/topologies/graphs_v2.txt ../out/mip_default_run$RUN mip.py default 30 5 2 2 $BASHFILE;;
