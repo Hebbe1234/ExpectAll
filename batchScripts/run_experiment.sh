4e9eda95c2af9199634c19f4e8817f4dcc66546a
fhyldg <fhyldg18@student.aau.dk>
2023-12-06 11:25:21 +0100

    experiment

--- batchScripts/run_experiment.sh
+++ batchScripts/run_experiment.sh
@@ -20,6 +20,8 @@ case $EXPERIMENT in
 		bash run_all.sh ../src ../src/topologies/topzoo/ ../src/topologies/graphs_v2.txt ../out/bdd_baseline_demands_effect_run$RUN run_bdd.py baseline 15 15 1 1 $BASHFILE;;
 	10.22) 
 		bash run_all.sh ../src ../src/topologies/topzoo/ ../src/topologies/graphs_v2.txt ../out/bdd_baseline_demands_effect_run2-2 run_bdd.py baseline 8 25 1 1 $BASHFILE;;
+	10.23)
+		bash run_all.sh ../src ../src/topologies/topzoo/ ../src/topologies/simple.txt ../out/bdd_baseline2.32 run_bdd.py baseline 8 25 1 1 $BASHFILE;;
 	10.3) 
 		bash run_all.sh ../src ../src/topologies/topzoo/ ../src/topologies/graphs_v2.txt ../out/bdd_baseline_wavelengths_effect_run$RUN run_bdd.py baseline 15 15 1 1 $BASHFILE;;
 	10.32) 
