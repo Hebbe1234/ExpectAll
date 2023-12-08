ec5d359e6a5cb3344f5a2b6083f7879aa758e876
Rasmus Hebsgaard <r.hebsgaard@gmail.com>
2023-12-08 09:13:10 +0100

    results

--- src/reordering_analysis.py
+++ src/reordering_analysis.py
@@ -62,9 +62,9 @@ def plot(df, order_by):
    
 def main():
     
-    results = pd.read_csv("../out/results.csv", delimiter=";")
-    results.to_pickle("../out/results.pkl")
-    df = pd.read_pickle("../out/results.pkl")
+    results = pd.read_csv("../out/results_edge_encoding.csv", delimiter=";")
+    results.to_pickle("../out/results_edge_encoding.pkl")
+    df = pd.read_pickle("../out/results_edge_encoding.pkl")
     df.columns = ["File", "ID", "Group_By_Edge_Order", "Generics_First", "Order", "Size", "Time"]
     df.Group_By_Edge_Order = df.Group_By_Edge_Order.astype(str).str.strip()
     df.Generics_First = df.Generics_First.astype(str).str.strip()
