141525d9f9aedfe6fc5a2c664790b60261d61f3f
fhyldg18 <57632309+fhyldg18@users.noreply.github.com>
2023-12-08 13:18:17 +0100

    little refac og grafer

--- src/mkplot/scatter.py
+++ src/mkplot/scatter.py
@@ -14,7 +14,7 @@ parser.add_argument("-s", default=";", type=str, help="seperator")
 parser.add_argument("-savedir", default="graphs/", help="dir to store")
 parser.add_argument("-savefile", default="default_graph", help = "file name to store")
 parser.add_argument("-split", default=9, type=int, help = "if all graphs on same: -1")
-parser.add_argument("-agg", default=None, type=list)
+parser.add_argument("-agg", default=None, type=int)
 args = parser.parse_args()
 
 if not os.path.isdir(args.savedir):
@@ -25,6 +25,23 @@ if not os.path.isdir(args.d):
     print("Directory does not exist: ",args.d)
     exit(0)
 
+def plotdf(df, xlabel, ylabel, x, y, save_dest):
+
+    df = df.sort_values(by=[x])
+    x = df.loc[:,x]
+    y = df.loc[:,y]
+
+    plt.plot(x,y,marker="o", ms=5)
+
+    xmin, xmax = plt.xlim()
+    plt.xticks(range(max(0,math.ceil(xmin)), math.ceil(xmax)))
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend(bbox_to_anchor=(1.02, 1.0))
+    plt.savefig(save_dest, bbox_inches = "tight")
+    plt.clf()
+    print(save_dest)
+    
 def plot(data, x_index, y_index, save_dest):
     xlabel = ""
     ylabel = ""
@@ -42,7 +59,7 @@ def plot(data, x_index, y_index, save_dest):
 
         xlabel = headers[x_index]
         ylabel = headers[y_index]
-    xmin, xmax = plt.xlim()
+    #xmin, xmax = plt.xlim()
     #plt.xticks(range(max(0,math.ceil(xmin)), math.ceil(xmax)))
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -53,44 +70,29 @@ def plot(data, x_index, y_index, save_dest):
 # for a given attribute we want to aggregate:
 #   For each value of that attribute
 #       Find row consisting of values from each graph
-def aggregate(data: dict[str, tuple[list ,list]], agg_list, x):
-    aggregated = {"aggregated": None}
-    
-    agg_headers = []
-    agg_row = [0 for i in range(len(agg_list))]
+def aggregate(data: dict[str, tuple[list ,list]], agg, x):    
     headers = []
-
     first = True
     df = pd.DataFrame()
+
     for graph, (headers, rows) in data.items():
         if first:
-            agg_headers = [i for i,h in enumerate(headers) if str(i) in agg_list]
             headers=headers
             first = False
         df2 = pd.DataFrame(rows)
         df = pd.concat([df, df2], ignore_index=True)
         df.reset_index()
 
-    f = {int(i) : "mean" for i in agg_list}
+    f = {agg : "median"}
     df = df.groupby(x, as_index=False).agg(f)
-    xaxis = headers[x]
-    yaxis = headers[int(agg_list[0])]
-
-    x = df.loc[:,x]
-    y = df.loc[:, int(agg_list[0])]
-
-    plt.plot(x,y,marker="o", ms=5, label="something")
-    plt.savefig(".", bbox_inches = "tight")
-
-    #df.columns = agg_headers
-    #print(df)
-    return aggregated
     
+    return df, headers
 
 # data is dict from graph name to pair of headers and rows
 data = convert_to_scatter_format(args.d)
-if args.agg:
-    aggregated = aggregate(data, args.agg, args.x)
+if args.agg >= 0:
+    df, headers = aggregate(data, args.agg, args.x)
+    plotdf(df, headers[args.x], headers[args.agg], args.x,args.agg,f"{args.savedir}{args.savefile}")
 else:
     num_graphs = 1 if args.split < 1 else math.ceil(len(data) / args.split)
     for i in range(num_graphs):
