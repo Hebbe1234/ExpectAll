536ed9254174720947b6284300bc746d05a7ecef
fhyldg18 <57632309+fhyldg18@users.noreply.github.com>
2024-02-15 11:30:52 +0100

    m

--- batchScripts/run_all.sh
+++ batchScripts/run_all.sh
@@ -21,7 +21,7 @@ jobs=""
 while read filename || [ -n "$filename" ]; do jobs+="$(bash $BASHFILE $SRC $TOPOLOGYPATH ${filename} $OUT $RUNFILE $EXPERIMENT $WAVELENGTHS $NUMBERDEMANDS $STARTDEMAND $INCREMENT),"; done < $DIR 
 
 echo $jobs >> fredstesting.txt
-jobs={$jobs%?}
+jobs= echo {$jobs%?}
 
 echo 
 echo "start run_all" >> fredstesting.txt
