ad2a02db29f305c4bdb0f025a2b22e4edcc468a1
fhyldg18 <57632309+fhyldg18@users.noreply.github.com>
2024-02-15 12:22:44 +0100

    bugfix

--- batchScripts/make_single_graph.sh
+++ batchScripts/make_single_graph.sh
@@ -8,7 +8,14 @@
 graph=$1
 out=$2
 
+cd ../src/mkplot
+source ../bdd_venv/bin/activate
+
+
 case $graph in
 	0.1)
 		python3 ../src/mkplot/AAU_create_json_and_make_pdfs.py --dirs $out --xaxis 0 --legend new
 esac
+
+deactivate
+
