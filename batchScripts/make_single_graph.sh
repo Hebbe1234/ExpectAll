00398e11a3a9034be1b14b0fc3565b38e19acc9b
fhyldg18 <57632309+fhyldg18@users.noreply.github.com>
2024-02-19 08:44:24 +0100

    bug

--- batchScripts/make_single_graph.sh
+++ batchScripts/make_single_graph.sh
@@ -14,7 +14,7 @@ source ../bdd_venv/bin/activate
 
 case $graph in
 	0.1)
-		python3 ../src/mkplot/make_cactus.py --dirs $out split_fancy3.tar.gz --xaxis 0 0 --legend new old --savedest ./cactus_graphs/superscript
+		python3 make_cactus.py --dirs $out split_fancy3.tar.gz --xaxis 0 0 --legend new old --savedest ./cactus_graphs/superscript
 esac
 
 deactivate
