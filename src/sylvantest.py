3a1eec4f49a42529a25c219b2202d2dbbf0b5ebb
fhyldg18 <57632309+fhyldg18@users.noreply.github.com>
2024-03-04 09:18:09 +0100

    test

--- src/sylvantest.py
+++ src/sylvantest.py
@@ -1,5 +1,11 @@
-from dd.sylvan import Function
-from dd.sylvan import BDD as _BDD
+try:
+	from dd.sylvan import Function
+	from dd.sylvan import BDD as _BDD
+except ImportError:
+	from dd.cudd import Function
+	from dd.cudd import BDD as _BDD
+	print("using cudd")
+
 
 bdd = _BDD()
 
@@ -7,4 +13,4 @@ bdd.declare("x","y","z")
 
 u = bdd.add_expr(r'x /\ y')
 
-v = bdd.exist(['x'], u)
+v = u.exist(*['x'])
