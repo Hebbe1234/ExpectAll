45699364e61b31608d186ff095da4f229dd4c10c
Martin <mpha19@student.aau.dk>
2024-03-13 13:35:53 +0100

    j

--- batchScripts/run_experiment.sh
+++ batchScripts/run_experiment.sh
@@ -39,7 +39,7 @@ case $EXPERIMENT in
 		bash run_all.sh ../src ../src/topologies/topzoo/ ../src/topologies/splitableNetworks/splitAble70.txt ../out/inc_parpar_limited_split_fancy_P3_v2$RUN run_bdd.py inc-par_limited_split_fancy_v2 3 2 10 5 $BASHFILE;;
 
 	2)
-		bash run_all.sh ../src ../src/topologies/japanese_topologies/ ../src/topologies/japanesetxt ../out/path_config_lim$RUN run_bdd.py path_config_lim 2 8 10 10 $BASHFILE;
+		bash run_all.sh ../src ../src/topologies/japanese_topologies/ ../src/topologies/japanese.txt ../out/path_config_lim$RUN run_bdd.py path_config_lim 2 8 10 10 $BASHFILE;
 
 
 
