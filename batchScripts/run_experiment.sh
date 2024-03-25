19509ccae298e7f6c4e1f04ad6fad87535524d64
Martin <mpha19@student.aau.dk>
2024-03-25 08:59:13 +0100

    l

--- batchScripts/run_experiment.sh
+++ batchScripts/run_experiment.sh
@@ -17,14 +17,14 @@ case $EXPERIMENT in
 	0.2) #This only tes thow long it takes to calculate sequential test. RSA_builder, have to be uniqly chagned for it to work :). 
 		outdir=seqonlydt$RUN
 		output=$(bash run_all.sh ../src ../src/topologies/japanese_topologies/ ../src/topologies/dt.txt ../out/$outdir run_bdd.py create_sequential_only_changed_rsa_builder_for_it_to_work $SEED 20 2 2 $BASHFILE);
-		echo $output; #not necessary, just to see jobs we await
-		sbatch --dependency=afterany:$output ./make_single_graph.sh $EXPERIMENT $outdir;; 
+		echo $output;; #not necessary, just to see jobs we await
+	#	sbatch --dependency=afterany:$output ./make_single_graph.sh $EXPERIMENT $outdir;; 
 
 	0.3) #This only tes thow long it takes to calculate sequential test. RSA_builder, have to be uniqly chagned for it to work :). 
 		outdir=seqonlykanto$RUN
 		output=$(bash run_all.sh ../src ../src/topologies/japanese_topologies/ ../src/topologies/kanto.txt ../out/$outdir run_bdd.py create_sequential_only_changed_rsa_builder_for_it_to_work $SEED 20 2 2 $BASHFILE);
-		echo $output; #not necessary, just to see jobs we await
-		sbatch --dependency=afterany:$output ./make_single_graph.sh $EXPERIMENT $outdir;; 
+		echo $output;; #not necessary, just to see jobs we await
+	#	sbatch --dependency=afterany:$output ./make_single_graph.sh $EXPERIMENT $outdir;; 
 
 	1.1)
 		bash run_all.sh ../src ../src/topologies/topzoo/ ../src/topologies/splitableNetworks/splitAble70.txt ../out/baseline_P2_v2$RUN run_bdd.py baseline_v2 2 2 10 5 $BASHFILE;
