60cc63302d64257af74ac62f54a756b07a23775b
Rasmus Hebsgaard <r.hebsgaard@gmail.com>
2024-03-27 23:10:08 +0100

    fix?

--- batchScripts/run_experiment.sh
+++ batchScripts/run_experiment.sh
@@ -111,7 +111,7 @@ done done done done done
 # Remove the last colon
 job_ids=("${job_ids[@]::${#job_ids[@]}-1}")
 
-echo "${job_ids[@]}" # Not necessary, just to see jobs we await
+echo "${job_ids[*]}" # Not necessary, just to see jobs we await
 sbatch --dependency=afterany:${job_ids[*]} ./make_single_graph.sh $EXPERIMENT $outdir
 
 
