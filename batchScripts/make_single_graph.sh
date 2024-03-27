ce6576466b17d031a23e4a33519f0f294d340407
Rasmus Hebsgaard <r.hebsgaard@gmail.com>
2024-03-27 22:54:41 +0100

    maybe working

--- batchScripts/make_single_graph.sh
+++ batchScripts/make_single_graph.sh
@@ -1,71 +1,46 @@
 #!/bin/bash
 
+# Configuration for Slurm job
 #SBATCH --mail-user=fhyldg18@student.aau.dk
 #SBATCH --mail-type=FAIL
 #SBATCH --partition=dhabi
 #SBATCH --mem=10G
 
+# Command-line arguments
 graph=$1
 out=$2
 
-cd ../src/mkplot
-source ../bdd_venv/bin/activate
+# Define common function to convert to CSV and plot scatter graphs
+convert_and_plot() {
+    local input_dir="../../out/$1"
+    local output_csv="csv/$1.csv"
+    python3 convert_to_csv.py -dir "$input_dir" -x 5 -savedest "$output_csv" -yfill 3600
+    python3 AAU_scatter.py -d "$output_csv" -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest "new_graphs/$1" -agg_func median
+}
 
+# Change directory and activate virtual environment
+cd ../src/mkplot || exit
+source ../bdd_venv/bin/activate || exit
 
+# Execute Python scripts based on the value of $graph
 case $graph in
-	0.1)
-		python3 make_cactus.py --dirs $out split_fancy3.tar.gz --xaxis 0 0 --legend new old --savedest ./cactus_graphs/superscript;;
-	0.2)
-		python3 convert_to_csv.py -dir ../../out/$out -x 5 -savedest csv/$out.csv -yfill 3600;
-        python3 AAU_scatter.py -d csv/$out.csv -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest new_graphs/$out -agg_func median;;
-	0.3)
-		python3 convert_to_csv.py -dir ../../out/$out -x 5 -savedest csv/one_path_dt.csv -yfill 3600;
-        python3 AAU_scatter.py -d csv/one_path_dt.csv -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest new_graphs/one_path_dt -agg_func median;;
-	0.4)
-		python3 convert_to_csv.py -dir ../../out/$out -x 5 -savedest csv/one_path_kanto_more_demands.csv -yfill 3600;
-        python3 AAU_scatter.py -d csv/one_path_kanto_more_demands.csv -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest new_graphs/one_path_kanto_more_demands -agg_func median;;
-
-	0.5)
-		python3 convert_to_csv.py -dir ../../out/$out -x 5 -savedest csv/$out.csv -yfill 3600;
-        python3 AAU_scatter.py -d csv/$out.csv -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest new_graphs/$out -agg_func median;;
-	0.6)
-		python3 convert_to_csv.py -dir ../../out/$out -x 5 -savedest csv/$out.csv -yfill 3600;
-        python3 AAU_scatter.py -d csv/$out.csv -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest new_graphs/$out -agg_func median;;
-	0.7)
-		python3 convert_to_csv.py -dir ../../out/$out -x 5 -savedest csv/$out.csv -yfill 3600;
-        python3 AAU_scatter.py -d csv/$out.csv -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest new_graphs/$out -agg_func median;;
-	0.8)
-		python3 convert_to_csv.py -dir ../../out/$out -x 5 -savedest csv/$out.csv -yfill 3600;
-        python3 AAU_scatter.py -d csv/$out.csv -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest new_graphs/$out -agg_func median;;
-
-
-	200)
-		python3 convert_to_csv.py -dir ../../out/kanto_one_path_lim_inc_size_1_demands${out}12 -x 5 -savedest csv/one_path_kanto_more_demands${out}12.csv -yfill 3600;
-        python3 AAU_scatter.py -d csv/one_path_kanto_more_demands${out}12.csv -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest new_graphs/one_path_kanto_more_demands${out}12 -agg_func median;;
-	300)
-		python3 convert_to_csv.py -dir ../../out/dt_one_path_lim_inc_size_1_demands${out}12 -x 5 -savedest csv/one_path_dt_more_demands${out}12.csv -yfill 3600;
-        python3 AAU_scatter.py -d csv/one_path_dt_more_demands${out}12.csv -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest new_graphs/one_path_dt_more_demands${out}12 -agg_func median;;
-	
-	6)
-		python3 make_cactus.py --dirs $out rsa_baseline.tar.gz --xaxis 0 0 --legend rsa_inc_par_lim rsa_baseline --savedest ./cactus_graphs/rsa_inc_par_lim_vs_baseline;;
-	6.1)
-		python3 make_cactus.py --dirs $out rsa_baseline.tar.gz --xaxis 0 0 --legend rsa_lim 		rsa_baseline --savedest ./cactus_graphs/rsa_lim_vs_baseline;;
-	6.2)
-		python3 make_cactus.py --dirs $out rsa_baseline.tar.gz --xaxis 0 0 --legend rsa_inc_par 	rsa_baseline --savedest ./cactus_graphs/rsa_inc_par_vs_baseline;;
-	6.3)
-		python3 make_cactus.py --dirs  rsa_inc_par_martin rsa_lim_martin rsa_inc_par_lim_martin rsa_baseline.tar.gz --xaxis 0 0 0 0  --legend inc_par lim inc_par_lim baseline --savedest ./cactus_graphs/rsa_inc_par_vs_baseline;;
-	7.1)
-		python3 make_cactus.py --dirs  rsa_seq_martin rsa_inc_par_seq_martin rsa_baseline.tar.gz --xaxis 0 0 0  --legend seq inc_par_seq baseline --savedest ./cactus_graphs/rsa_inc_seq_vs_baseline;;
-	8.1)
-		python3 make_cactus.py --dirs  sequential_P2_v22 rsa_baseline.tar.gz --xaxis 0 0  --legend seq baseline --savedest ./cactus_graphs/rsa_seq_vs_baseline;;
-	
-	411.0)
-		python3 convert_to_csv.py -dir ../../out/$out -x 5 -savedest csv/$out.csv -yfill 3600;
-        python3 AAU_scatter.py -d csv/$out.csv -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest new_graphs/$out -agg_func median;;
-	411.1)
-		python3 convert_to_csv.py -dir ../../out/$out -x 5 -savedest csv/$out.csv -yfill 3600;
-        python3 AAU_scatter.py -d csv/$out.csv -xlabel Demands -ylabel "Run time (s)" -agg 0 -x 5 -savedest new_graphs/$out -agg_func median;;
+    0.1)
+        python3 make_cactus.py --dirs "$out" split_fancy3.tar.gz --xaxis 0 0 --legend new old --savedest ./cactus_graphs/superscript ;;
+    0.2|0.3|0.4|0.5|0.6|0.7|0.8|411.0|411.1)
+        convert_and_plot "$out" ;;
+    200)
+        convert_and_plot "one_path_kanto_more_demands${out}12" ;;
+    300)
+        convert_and_plot "one_path_dt_more_demands${out}12" ;;
+    6|6.1|6.2|6.3)
+        python3 make_cactus.py --dirs "$out" rsa_baseline.tar.gz --xaxis 0 0 --legend rsa_inc_par_lim rsa_baseline --savedest ./cactus_graphs/rsa_inc_par_lim_vs_baseline ;;
+    7.1)
+        python3 make_cactus.py --dirs rsa_seq_martin rsa_inc_par_seq_martin rsa_baseline.tar.gz --xaxis 0 0 0 --legend seq inc_par_seq baseline --savedest ./cactus_graphs/rsa_inc_seq_vs_baseline ;;
+    8.1)
+        python3 make_cactus.py --dirs sequential_P2_v22 rsa_baseline.tar.gz --xaxis 0 0 --legend seq baseline --savedest ./cactus_graphs/rsa_seq_vs_baseline ;;
+    *)
+        echo "Unsupported graph value: $graph" ;;
 esac
 
+# Deactivate virtual environment
 deactivate
-
--- batchScripts/run_single.sh
+++ batchScripts/run_single.sh
@@ -10,25 +10,11 @@
 let "m=1024*1024*50"
 ulimit -v $m
 
-SRC=$1
-TOPOLOGYPATH=$2
-FILENAME=$3
-OUTPUT=$4
-WAVELENGTHS=$5
-DEMANDS=$6
-RUNFILE=$7
-EXPERIMENT=$8
-p1=${9}
-p2=${10}
-p3=${11}
-p4=${12}
-p5=${13}
 
 # Create and activate a virtual environment
-source $SRC/bdd_venv/bin/activate
-echo "${FILENAME};"
+source ../src/bdd_venv/bin/activate
 # Run your Python script
-python3 -u $SRC/$RUNFILE --experiment=$EXPERIMENT --filename=$TOPOLOGYPATH$FILENAME --wavelengths=$WAVELENGTHS --demands=$DEMANDS --par1=$p1 --par2=$p2 --par3=$p3 --par4=$p4 --par5=$p5 > $OUTPUT
+python3 -u "${@}"
 
 
 # Deactivate the virtual environment
