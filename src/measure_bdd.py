46ee5e612cca785ae53a781cc7438c2596b1604f
Rasmus Hebsgaard <r.hebsgaard@gmail.com>
2024-04-10 12:01:34 +0200

    measurement

--- src/measure_bdd.py
+++ src/measure_bdd.py
@@ -1,3 +1,6 @@
+import functools
+import json
+from multiprocessing import Pool
 import os
 import pickle
 from typing import Callable
@@ -17,31 +20,44 @@ except ImportError:
 
    print("Using autoref... ")
 
-def load_for_measurement(dir, measure: Callable):
+def measurement(filename, root, data_dir, bdd_dir, results_dir, measure_key, measure):
+    # Print the full path of the file
+    print(os.path.join(root, filename))
+    id = filename.split(".")[0]
+
+    base_path = os.path.join(data_dir,f"{id}_base.pickle") 
+    bdd_file = os.path.join(bdd_dir,f"{id}.json")
+    result_file = os.path.join(results_dir,f"{id}.json")
+    
+    with open(base_path, "rb") as base_file:
+        base = pickle.load(base_file)
+        bdd = _BDD()
+        roots = bdd.load(bdd_file)
+        print(f'Loaded BDD: {roots}')  
+        base.bdd = bdd  
+        
+        res = {}
+        with open(result_file, "r") as jsonFile:
+            res = json.load(jsonFile)[0]
+
+        res[measure_key] = measure(base, roots[0])
+        print(id, res[measure_key])
+        
+        with open(result_file, "w") as jsonFile:
+            json.dump([res], jsonFile, indent=4)
+    
+    
+    
+    
+def load_for_measurement(dir, measure_key: str, measure: Callable):
     
     bdd_dir = os.path.join(dir, "bdds")
     data_dir =  os.path.join(dir, "data")
     results_dir = os.path.join(dir, "results")
-    
+ 
     for root, dirs, files in os.walk(bdd_dir):
         for filename in files:
-            # Print the full path of the file
-            print(os.path.join(root, filename))
-            id = filename.split(".")[0]
-        
-            base_path = os.path.join(data_dir,f"{id}_base.pickle") 
-            bdd_file = os.path.join(bdd_dir,f"{id}.json")
-            
-            with open(base_path, "rb") as base_file:
-                base = pickle.load(base_file)
-                bdd = _BDD()
-                roots = bdd.load(bdd_file)
-                print(f'Loaded BDD: {roots}')  
-                base.bdd = bdd  
-                
-                meas = measure(base, roots[0])
-                print(id, meas)
-         
+            measurement(filename, root=root, data_dir=data_dir, bdd_dir=bdd_dir, results_dir=results_dir, measure_key=measure_key, measure=measure)
 
 class SolutionBlock():
     def __init__(self, expr):
@@ -70,4 +86,4 @@ def edge_evaluation(base, expr, k, is_dynamic_vars):
             solved_edges += 1
     return solved_edges, total_edges, (solved_edges * 100)/total_edges
 
-load_for_measurement("../out/EXPERIMENT_0_7_RUN_2", usage)
+load_for_measurement("../out/EXPERIMENT_0_7_RUN_2", "usage", usage)
