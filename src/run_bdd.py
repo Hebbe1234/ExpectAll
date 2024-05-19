10c23c492418de339c55094ca37e7ab8a63c89b2
fhyldg18 <57632309+fhyldg18@users.noreply.github.com>
2024-05-19 21:17:18 +0200

    no population

--- src/run_bdd.py
+++ src/run_bdd.py
@@ -129,6 +129,9 @@ if __name__ == "__main__":
     elif args.experiment in ["topzoo_mip_1", "topozoo_gap_free_safe_limited_heuristic_upper_bound"]:
         demands = get_gravity_demands_no_population(G, args.demands,multiplier=1, seed=seed)
         print("no population: using other gravity demand method")
+    elif p5 == "no population":
+        demands = get_gravity_demands_no_population(G, args.demands,multiplier=1, seed=seed)
+        print("no population: using other gravity demand method")
     else:
         demands = get_gravity_demands(G, args.demands,multiplier=1, seed=seed)
     
