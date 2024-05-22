c42ffe54f665024c15fd16a4c6277a42af0eeba0
Martin <mpha19@student.aau.dk>
2024-05-22 08:27:32 +0200

    l

--- src/topologies/chatGPTDraw/drawBoxPLot/makeBoxPlot.py
+++ src/topologies/chatGPTDraw/drawBoxPLot/makeBoxPlot.py
@@ -6,28 +6,36 @@ import matplotlib.pyplot as plt
 for i in range(1,4): 
     plt.clf()
     print(i)
-    folder_name = "json2"
+    folder_name1 = "json1"
+    folder_name2 = "json2"
     edgeFail = i
     file_name = "EdgeFailover_"+str(edgeFail)+".json"  # Change this to your desired file name
 
     # Path to the folder containing JSON files
-    folder_path = os.path.join(os.getcwd(), folder_name)
+    folder_path1 = os.path.join(os.getcwd(), folder_name1)
+    folder_path2 = os.path.join(os.getcwd(), folder_name2)
 
     # Path to the selected JSON file
-    file_path = os.path.join(folder_path, file_name)
+    file_path1 = os.path.join(folder_path1, file_name)
+    file_path2 = os.path.join(folder_path2, file_name)
 
     # Open the JSON file and load the data
-    with open(file_path, 'r') as file:
-        data = json.load(file)
+    with open(file_path1, 'r') as file:
+        data1 = json.load(file)
+    with open(file_path2, 'r') as file:
+        data2 = json.load(file)
 
     # Sort data based on the "demands" value
-    data.sort(key=lambda x: x['demands'])
+    data1.sort(key=lambda x: x['demands'])
+    data2.sort(key=lambda x: x['demands'])
 
     # Extracting the data into a list of lists
-    data_values = [item['all_times'][0] for item in data]
+    data_values1 = [item['all_times'][0] for item in data1]
+    data_values2 = [item['all_times'][0] for item in data2]
 
     # Creating the boxplot
-    plt.boxplot(data_values, whis=(0,100))
+    plt.boxplot(data_values1, whis=(0,100))
+    plt.boxplot(data_values2, whis=(0,100))
 
     # Adding labels
     plt.xlabel('demands')
@@ -35,13 +43,12 @@ for i in range(1,4):
     plt.title('Boxplot of Data')
 
     # Setting x-axis ticks
-    plt.xticks(range(1, len(data) + 1), [item['demands'] for item in data])
+    plt.xticks(range(1, len(data1) + 1), [item['demands'] for item in data1])
 
     # Setting y-axis lower limit to 0
-    plt.ylim(bottom=0)
 
     # Save the boxplot as a PNG file
-    output_folder = "kantoEdgeTop"
+    output_folder = "AAAA"
     output_file_path = os.path.join(os.getcwd(), output_folder, file_name.replace(".json", "_boxplot.png"))
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
