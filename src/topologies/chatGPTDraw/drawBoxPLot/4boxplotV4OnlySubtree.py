fe3ced734aeb09b9eb98e2c636e85d827ed2a21d
Martin <mpha19@student.aau.dk>
2024-05-29 09:49:27 +0200

    Chagnes to MIp super

--- src/topologies/chatGPTDraw/drawBoxPLot/4boxplotV4OnlySubtree.py
+++ src/topologies/chatGPTDraw/drawBoxPLot/4boxplotV4OnlySubtree.py
@@ -85,8 +85,8 @@ for topologyName in topology2:
 
         # Setting y-axis lower limit to 0
         plt.yscale("log")
-        plt.axhline(y=50, color='gray', linestyle='--')
-        plt.axhline(y=200, color='gray', linestyle='-.')
+        plt.axhline(y=0.050, color='gray', linestyle='--')
+        plt.axhline(y=0.200, color='gray', linestyle='-.')
 
         # Creating custom legend
         legend_elements = [
